54830df5796ad9e8b1286a2a93128c7a8db60443
Jeremy Ha <jorm@Jeremys-MacBook-Pro.local>
2025-07-25 00:52:41 -0700

    port flask

--- backend/app.py
+++ backend/app.py
@@ -159,4 +159,5 @@ def api_create_playlist():
         })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
